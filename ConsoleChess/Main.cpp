de764035914cc4a7451e6420ffea51aa4bba9cb5
Jacob Bright <54848093+jbr1gh7@users.noreply.github.com>
2021-10-11 21:56:52 +0000

    removed hard coded vals, neatening things up, this needs to be wrapped into its own function

--- ConsoleChess/Main.cpp
+++ ConsoleChess/Main.cpp
@@ -2,39 +2,48 @@
 #include <string> 
 
 const int gridAmount = 8;
-const int xSpacing = 10;
+const int yOverflow = 9; //9 vertical lines add extra space that needs to be accounted for when drawing horizontal line
+const int xSpacing = 8;
 const int ySpacing = 3;
 
 void out(std::string message) {
     std::cout << message;
 }
 
+std::string generateWhitespace(int spacing) {
+    std::string whitespace = "";
+    for (int i = 0; i < spacing; i ++) {
+        whitespace += " ";
+    }
+
+    return whitespace;
+}
+
 void printXGridline() {
-    for (int i=0; i<gridAmount*xSpacing+1; i++) {
+    for (int i=0; i < gridAmount * xSpacing + yOverflow; i++) {
         out("―");
     } 
 }
 
 void printYGridline() {
-    for (int i=0; i<ySpacing; i++) {
+    std::string spacing = generateWhitespace(gridAmount);
+
+    for (int i = 0; i < ySpacing; i++) {
         out("|");
-        for (int j=0; j<gridAmount; j++) {
-            out("         |");
+
+        for (int j = 0; j < gridAmount; j++) {
+            out(spacing + "|");
         }
+
         out("\n");
     }
-
 }
 
 int main()
 {
-
-
-    for (int i=0; i<gridAmount; i++) {
+    for (int i = 0; i < gridAmount; i++) {
         printXGridline();
-
         out("\n");
-
         printYGridline();
     }
 
