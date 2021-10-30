7d41f2db534574705f3a177758187493b4f17472
Jacob Bright <54848093+jbr1gh7@users.noreply.github.com>
2021-10-30 01:44:02 +0000

    that messed everything up

--- ConsoleChess/Main.cpp
+++ ConsoleChess/Main.cpp
@@ -55,12 +55,6 @@ TODO (in order):
 #include <filesystem>
 
 #include "Headers/BoardItem.h"
-#include "Headers/validateInput.h"
-#include "Headers/inputOutput.h"
-#include "Headers/drawGrid.h"
-#include "Headers/generateBoard.h"
-#include "Headers/interactBoard.h"
-#include "Headers/readWriteLoad.h"
 
 const int squaresOnAxes = 8;
 const int gridSpacing = 6;
@@ -79,6 +73,167 @@ int itemInRowCount = 0;
 
 std::string currentFile;
 
+void out(std::string message) {
+    std::cout << message;
+}
+
+std::string input(std::string message) {
+    std::string put;
+
+    out(message);
+    std::getline(std::cin, put);
+    return put;
+}
+
+std::string generateIconSpacing(int spacing) {
+    std::string whitespace = "";
+    for (int i = 0; i < spacing; i++) {
+        whitespace += " ";
+    }
+
+    return whitespace;
+}
+
+void printXLabels() {
+    std::string spacing = generateIconSpacing(gridSpacing / 2);
+    std::vector labels = {"a", "b", "c", "d", "e", "f", "g", "h"};
+    int labelIndex = 0;
+
+    for (int i=0; i < (gridSpacing * xWidth) + (squaresOnAxes * pieceOverflow) + yLineOverflow; i++) {
+        if (i % squaresOnAxes == 0 && i != 0) {
+            out(" " + spacing + labels[labelIndex] + spacing);
+            labelIndex ++;
+        }
+    }
+
+    out("\n");
+}
+
+void printXGridline() {
+    for (int i=0; i < (gridSpacing * xWidth) + (squaresOnAxes * pieceOverflow) + yLineOverflow; i++) {
+        out("―");
+    }
+
+    out("\n");
+}
+
+void printYGridline(std::vector<std::vector<BoardItem>> board) {
+    std::string spacing;
+    int midPoint = 1;
+
+    for (int i = 0; i < yLength; i++) {
+        out("|");
+
+        for (int j = 0; j < squaresOnAxes; j++) {
+            spacing = generateIconSpacing(gridSpacing + pieceOverflow);
+
+            if (i == midPoint) {
+                spacing = generateIconSpacing(gridSpacing / 2);
+                out(spacing + board[rowCount][itemInRowCount].icon);
+
+                itemInRowCount ++;
+
+                if (itemInRowCount % squaresOnAxes == 0) {
+                    rowCount ++;
+                    itemInRowCount = 0;
+                }
+            }
+
+            out(spacing + "|");
+        }
+
+        if (i == midPoint) {
+            out(spacing + std::to_string(9-rowCount)); //9 inverts numbers
+        }
+
+        out("\n");
+    }
+}
+
+void drawBoard(std::vector<std::vector<BoardItem>> board, bool isAnimated = false) {
+    for (int i = 0; i < squaresOnAxes; i++) {
+        printXGridline();
+
+        if (isAnimated) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(15));
+        }
+
+        printYGridline(board);
+    }
+
+    printXGridline();
+    printXLabels();
+
+    rowCount = 0;
+    itemInRowCount = 0;
+}
+
+void clearAndDraw(std::vector<std::vector<BoardItem>> board, bool isAnimated = false) {
+    system("clear");
+    drawBoard(board, isAnimated);
+}
+
+std::vector<BoardItem> generateMajorPieceRow(bool isDark) {
+    std::vector<BoardItem> majorPieceRow;
+    std::vector<std::string> names = {"ROOK", "KNHT", "BSHP", "QUEN", "KING", "BSHP", "KNHT", "ROOK"};
+
+    for (int i = 0; i < names.size(); i++) {
+        BoardItem boardItem;
+        boardItem.isDark = isDark;
+        boardItem.setName(names[i]);
+        boardItem.setIcon();
+        majorPieceRow.push_back(boardItem);
+    }
+
+    return majorPieceRow;
+}
+
+std::vector<BoardItem> generatePawnRow(bool isDark) {
+    std::vector<BoardItem> pawnRow;
+
+    for (int i = 0; i < squaresOnAxes; i++) {
+        BoardItem boardItem;
+        boardItem.isDark = isDark;
+        boardItem.setName("PAWN");
+        boardItem.setIcon();
+        pawnRow.push_back(boardItem);
+    }
+
+    return pawnRow;
+}
+
+std::vector<BoardItem> generateEmptyRow() {
+    std::vector<BoardItem> emptyRow;
+
+    for (int i = 0; i < squaresOnAxes; i++) {
+        BoardItem boardItem;
+        boardItem.setName("EMTY");
+        boardItem.setIcon();
+        emptyRow.push_back(boardItem);
+    }
+
+    return emptyRow;
+}
+
+std::vector<std::vector<BoardItem>> initialiseBoardStructure() {
+    std::vector<std::vector<BoardItem>> board;
+    int emptyRowAmount = 4;
+
+    board.push_back(generateMajorPieceRow(true));
+    board.push_back(generatePawnRow(true));
+    //board.push_back(generateEmptyRow());
+
+    for (int i = 0; i < emptyRowAmount; i++) {
+        board.push_back(generateEmptyRow());
+    }
+
+    //board.push_back(generateEmptyRow());
+    board.push_back(generatePawnRow(false));
+    board.push_back(generateMajorPieceRow(false));
+
+    return board;
+}
+
 void outputSaves(std::vector<std::filesystem::path> fileVector) {
     int counter = 0;
 
@@ -89,6 +244,304 @@ void outputSaves(std::vector<std::filesystem::path> fileVector) {
         out(std::to_string(counter) + ". " + cleaned);
         counter ++;
     }
+}
+
+std::pair<int, int> findIndexInVector(std::vector<std::vector<BoardItem>> board, std::string position) {
+    std::pair<int, int> indices;
+    indices.first = -1;
+    indices.second = -1;
+
+    for (int i = 0; i < board.size(); i++) {
+        for (int j = 0; j < board[i].size(); j++) {
+            if (board[i][j].position == position) {
+                indices.first = i; //y
+                indices.second = j; //x
+
+                return indices;
+            }
+        }
+    }
+
+    return indices;
+}
+
+std::vector<std::vector<BoardItem>> swap(std::vector<std::vector<BoardItem>> board, std::string positionFrom, std::string positionTo) {
+    std::pair<int, int> indexFrom = findIndexInVector(board, positionFrom);
+    std::pair<int, int> indexTo = findIndexInVector(board, positionTo);
+    BoardItem temp = board[indexTo.first][indexTo.second];
+    
+    board[indexTo.first][indexTo.second] = board[indexFrom.first][indexFrom.second];
+    board[indexTo.first][indexTo.second].position = positionTo;
+
+    board[indexFrom.first][indexFrom.second] = temp;
+    board[indexFrom.first][indexFrom.second].position = positionFrom;
+
+    return board;
+}
+
+std::vector<std::vector<BoardItem>> take(std::vector<std::vector<BoardItem>> board, std::string positionFrom, std::string positionTo) {
+    std::pair<int, int> indexFrom = findIndexInVector(board, positionFrom);
+    std::pair<int, int> indexTo = findIndexInVector(board, positionTo);
+    
+    BoardItem empty;
+    empty.setName("EMTY");
+    empty.setIcon();
+    
+    board[indexTo.first][indexTo.second] = board[indexFrom.first][indexFrom.second];
+    board[indexTo.first][indexTo.second].position = positionTo;
+
+    board[indexFrom.first][indexFrom.second] = empty;
+    board[indexFrom.first][indexFrom.second].position = positionFrom;
+
+    return board;
+}
+
+std::vector<std::vector<BoardItem>> assignPositions(std::vector<std::vector<BoardItem>> board) {
+    std::vector<std::string> xAxisLabels = {"a", "b", "c", "d", "e", "f", "g", "h"};
+    std::vector<std::string> yAxisLabels = {"8", "7", "6", "5", "4", "3", "2", "1"}; //backwards to assign smallest from bottom to top
+
+
+    for (int i = 0; i < board.size(); i++) {
+        for (int j = 0; j < board[i].size(); j++) {
+            board[i][j].position = xAxisLabels[j] + yAxisLabels[i];
+        }
+    }
+
+    return board;
+}
+
+int findLargest(std::pair<int, int> vector) {
+    if ((std::abs(vector.first) == std::abs(vector.second)) || (std::abs(vector.first) > std::abs(vector.second))) {
+        return std::abs(vector.first);
+    }
+
+    return std::abs(vector.second);
+}
+
+bool isPositive(int toFind) {
+    if (toFind > 0) 
+        return true;
+    else
+        return false;
+}
+
+bool stepThrough(std::pair<int, int> currentPosition, std::pair<int, int> desiredPosition, std::pair<int, int> vector, std::vector<std::vector<BoardItem>> board) {
+    int largestAbsolute = findLargest(vector);
+    int stepX = 0;
+    int stepY = 0;
+    
+    for (int i = 0; i < largestAbsolute; i++) {
+        if (vector.first != 0 && vector.first != stepX) {
+            if (isPositive(vector.first)) 
+                stepX ++;
+            else 
+                stepX --;
+        }
+
+        if (vector.second != 0 && vector.second != stepY) {
+            if (isPositive(vector.second)) 
+                stepY ++;
+            else 
+                stepY --;
+        }
+
+        if (currentPosition.second + stepX != desiredPosition.second || currentPosition.first - stepY != desiredPosition.first) {
+            if (board[currentPosition.first - stepY][currentPosition.second + stepX].name != "EMTY") {
+                return false;
+            }
+        }
+        else if (currentPosition.second + stepX == desiredPosition.second && currentPosition.first - stepY == desiredPosition.first) {
+            return true;
+        }
+    }
+}
+
+std::string boolToString(bool b) {
+  return b ? "true" : "false";
+} 
+
+void updateSave(std::vector<std::vector<BoardItem>> board, bool isDarkTurn) {
+    std::ofstream save(currentFile, std::ofstream::out | std::ofstream::trunc); //trunc deletes contents
+    
+    for (int i = 0; i < board.size(); i++) {
+        for (int j = 0; j < board[i].size(); j++) {
+            save << board[i][j].name + "." + boolToString(board[i][j].isDark) + " ";
+        }
+
+        save << "\n";
+    }
+
+    for (int i = 0; i < takenPieces.size(); i++) {
+        for (int j = 0; j < takenPieces[i].size(); j++) {
+            save << takenPieces[i][j] + " ";
+        }
+
+        save << "\n";
+    } 
+
+    save << boolToString(isDarkTurn);
+    save.close();
+}
+
+void newSave(std::vector<std::vector<BoardItem>> board, bool isDarkTurn) {
+    time_t present = time(0);
+    char* presentDate = ctime(&present);
+    std::string dateString(presentDate);
+
+    currentFile = "ConsoleChess/SaveFiles/" + dateString + ".txt";
+    std::ofstream save(currentFile);
+    save.close();
+
+    updateSave(board, isDarkTurn);
+}
+
+std::vector<std::vector<std::string>> readFile2d(std::string fileName) {
+    std::ifstream file(fileName);
+    std::string line;
+    std::vector<std::vector<std::string>> entries2d;
+  
+    while(getline(file, line)) {
+        std::stringstream stream(line);
+        std::string field;
+        std::vector<std::string> rowList;
+
+        while(getline(stream, field, ' ')) {
+            rowList.push_back(field);
+        }
+
+        entries2d.push_back(rowList);        
+    }
+
+    file.close();
+    return entries2d;
+}
+
+std::vector<std::filesystem::path> getPathsInFolder() {
+    std::vector<std::filesystem::path> fileVector;
+
+    for (const auto & file : std::filesystem::directory_iterator("ConsoleChess/SaveFiles/")) {
+        std::filesystem::path path = file.path();
+        fileVector.push_back(path);
+    }
+
+    return fileVector;
+}
+
+std::vector<std::vector<BoardItem>> loadBoard(std::vector<std::vector<BoardItem>> board, std::vector<std::vector<std::string>> dataGrid) {
+    for (int i = 0; i < squaresOnAxes; i++) {
+        std::vector<BoardItem> row;
+
+        for (int j = 0; j < squaresOnAxes; j++) {
+            BoardItem boardItem;
+            boardItem.setName(dataGrid[i][j].substr(0, 4));
+
+            if (dataGrid[i][j].substr(5) == "true")
+                boardItem.isDark = true;
+            else
+                boardItem.isDark = false;
+
+            boardItem.setIcon();
+            row.push_back(boardItem);
+        }
+                                    
+        board.push_back(row);
+    }
+
+    board = assignPositions(board);
+    return board;
+}
+
+std::string lower(std::string toBeLowered) {
+    std::string lowerString = "";
+    std::smatch matchLetters;
+    std::regex lettersExpression("[^A-H]");
+    
+    for (int i = 0; i < toBeLowered.size(); i++) {
+        if (std::regex_search(toBeLowered, matchLetters, lettersExpression)) {
+            char lowered = tolower(toBeLowered[i]);
+            lowerString += lowered;
+        }
+        else {
+            lowerString += toBeLowered[i];
+        }
+    }
+
+    return lowerString;    
+}
+
+bool validateLength(std::string input) {
+    int acceptedLength = 4; // 2 chars each position, 1 char for whitespace
+
+    if (input.size() == acceptedLength) {
+        return true;
+    } 
+
+    return false;
+}
+
+bool validateNoMove(std::string input) {
+    if (input.substr(0, 2) != input.substr(2, 2)) {
+        return true;
+    } 
+
+    return false;
+}
+
+bool validateNumber(std::string toBeValidated) {
+    std:: smatch matchLetters;
+    std::regex lettersExpression("[^0-9]"); 
+
+    if (std::regex_search(toBeValidated, matchLetters, lettersExpression))
+        return false;
+    
+    return true;
+}
+
+bool validateChars(std::string input) {
+    std::smatch matchChars;
+    std::regex charsExpression("[^0-8] [^a-h]");
+
+    if (std::regex_search(input, matchChars, charsExpression)) {
+        return false;
+    }
+
+    std::regex digitsExpression("[^0-8]");
+
+    std::ptrdiff_t digitOccurences(std::distance(
+        std::sregex_iterator(input.begin(), input.end(), digitsExpression),
+        std::sregex_iterator())
+    );
+
+    std::regex lettersExpression("[^a-h]");
+
+    std::ptrdiff_t lettersOccurences(std::distance(
+        std::sregex_iterator(input.begin(), input.end(), lettersExpression),
+        std::sregex_iterator())
+    );
+    
+    if (digitOccurences > 2 || lettersOccurences > 2) {
+        return false;
+    }
+
+    std::smatch matchDigits;
+
+    std::string inputSecond = input.substr(1, 1);
+    std::string inputFourth = input.substr(3, 1);
+
+    if (std::regex_search(inputSecond, matchDigits, digitsExpression) || std::regex_search(inputFourth, matchDigits, digitsExpression)) {
+        return false;
+    }
+
+    std::smatch matchLetters;
+    
+    std::string inputFirst = input.substr(0, 1);
+    std::string inputThird = input.substr(2, 1);
+
+    if (std::regex_search(inputFirst, matchLetters, lettersExpression) || std::regex_search(inputThird, matchLetters, lettersExpression)) {
+        return false;
+    }
+    
+    return true;
 }
 
 bool coinToss() {
